eec8386b4b3f731d296483d243af2a59f3c39b3c
FlamerMC <153970526+FlamerMC@users.noreply.github.com>
2024-02-24 19:24:45 -0500

    Add the installer script

---
+++ Flabian.sh
@@ -0,0 +1,17 @@
+echo '
+███████╗██╗░░░░░░█████╗░██████╗░░█████╗░██╗███╗░░██╗
+██╔════╝██║░░░░░██╔══██╗██╔══██╗██╔══██╗██║████╗░██║
+█████╗░░██║░░░░░███████║██████╦╝███████║██║██╔██╗██║
+██╔══╝░░██║░░░░░██╔══██║██╔══██╗██╔══██║██║██║╚████║
+██║░░░░░███████╗██║░░██║██████╦╝██║░░██║██║██║░╚███║
+╚═╝░░░░░╚══════╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝ Plasma'
+echo this will install it.
+sudo apt update
+sudo apt install snapd
+sudo snap install core
+# sudo apt install kde-plasma-desktop -y
+sudo snap install steam
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.usebottles.bottles
